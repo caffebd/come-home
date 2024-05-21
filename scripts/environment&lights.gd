cdef62644574559d3c84730fd41de03c852e5623
Foyezes <106163842+Foyezes@users.noreply.github.com>
2024-05-21 21:18:56 +0600

    g

--- scripts/environment&lights.gd
+++ scripts/environment&lights.gd
@@ -24,19 +24,9 @@ func _time_transition_0():
 	tween.tween_property($WorldEnvironment, "environment:volumetric_fog_albedo", Color("3c3c3c"), 2.0)
 	tween.tween_property($WorldEnvironment, "environment:volumetric_fog_emission", Color("0f2722"), 2.0)
 	tween.tween_property($WorldEnvironment, "environment:volumetric_fog_density", 0.05, 2.0)
-	tween.tween_property($sun, "light_energy", 0.0, 2.0)
-	tween.tween_property($moon, "light_energy", 0.08, 2.0)
 
 func _time_transition_1():
-	var tween = create_tween().parallel()
-	tween.tween_property($WorldEnvironment, "environment:sky:sky_material:sky_top_color", Color("1c4a53"), 2.0)
-	tween.tween_property($WorldEnvironment, "environment:sky:sky_material:sky_horizon_color", Color("aa6a4c"), 2.0)
-	tween.tween_property($WorldEnvironment, "environment:sky:sky_material:ground_horizon_color", Color("aa6a4c"), 2.0)
-	tween.tween_property($WorldEnvironment, "environment:volumetric_fog_albedo", Color("ffffff"), 2.0)
-	tween.tween_property($WorldEnvironment, "environment:volumetric_fog_emission", Color("77604a"), 2.0)
-	tween.tween_property($WorldEnvironment, "environment:volumetric_fog_density", 0.0265, 2.0)
-	tween.tween_property($sun, "light_energy", 0.17, 2.0)
-	tween.tween_property($moon, "light_energy", 0.089, 2.0)
+	pass
 
 
 
